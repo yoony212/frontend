c936f29c8b2b98404a6eabcb144ddc3293d71e03
Yoontaek Yim <Imu.Yunteku@trans-cosmos.co.jp>
2023-05-23 22:52:00 +0900

    fix: main.js

--- web-tab/main.js
+++ web-tab/main.js
@@ -2,24 +2,24 @@ const showTab = (selector) => {
   $('.tabs-menu div').removeClass('active');
   $('.tabs-content div').hide();
   $(`.tabs-menu div[id="${selector}"]`).addClass('active');
-  if(selector === 'tab-menu-a') {
-    $('.tabs-a').show();
-    $('.tabs-b').hide();
-    $('.tabs-c').hide();
-  } else if(selector === 'tab-menu-b') {
-    $('.tabs-a').hide();
-    $('.tabs-b').show();
-    $('.tabs-c').hide();
-  } else if(selector === 'tab-menu-c') {
-    $('.tabs-a').hide();
-    $('.tabs-b').hide();
-    $('.tabs-c').show();
+  if (selector === 'tab-menu-a') {
+    $('#tabs-a').show();
+    $('#tabs-b').hide();
+    $('#tabs-c').hide();
+  } else if (selector === 'tab-menu-b') {
+    $('#tabs-a').hide();
+    $('#tabs-b').show();
+    $('#tabs-c').hide();
+  } else if (selector === 'tab-menu-c') {
+    $('#tabs-a').hide();
+    $('#tabs-b').hide();
+    $('#tabs-c').show();
   }
 };
 
 $('.tabs-menu div').on('click', (e) => {
   const selector = $(e.target).attr('id');
   showTab(selector);
-}); 
+});
 
 showTab('tab-menu-a');

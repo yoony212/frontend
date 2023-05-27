50a72e835f261c1921e235f0f0a3a5995f42fc13
Yoontaek Yim <Imu.Yunteku@trans-cosmos.co.jp>
2023-05-27 23:58:27 +0900

    start vue-kadai3

--- memo/memo.js
+++ memo/memo.js
@@ -1,15 +1,25 @@
-let password_length = 8;
-let letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-function getPW() {
-  function genLetter() {
-    let length = Math.floor(Math.random() * 62);
-    return letters[length];
-  }
-  let pw = [];
-  for (let i = 0; i < password_length; i++) {
-    let letter = genLetter();
-    pw[i] = letter;
-  }
-  return pw.join('');
-}
-console.log(getPW());
+const ButtonPreference = {
+  props: ['initialCount'],
+  data() {
+    return {
+      count: this.initialCount,
+    };
+  },
+  methods: {
+    countUp() {
+      this.initialCount += 1;
+    },
+  },
+  template: `
+    <button v-on:click="countUp">
+      {{ initialCount }} いいね！
+    </button>
+  `,
+};
+
+new Vue({
+  el: '#example',
+  components: {
+    'button-preference': ButtonPreference,
+  },
+});
